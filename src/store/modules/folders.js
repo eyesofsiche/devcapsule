f4bb07df1e96d1eddc7b0499ebb7ee5f65f81303
siche <siche@eos.pe.kr>
2025-04-29 14:08:24 +0900

    db, stroe 설정

---
+++ src/store/modules/folders.js
@@ -0,0 +1,27 @@
+import _ from "lodash";
+
+const state = {
+  list: [],
+};
+
+const mutations = {
+  SET_LIST: (state, list) => {
+    state.list = list;
+  },
+};
+
+const actions = {
+  setList({ commit }, { list, options = { save: true } }) {
+    commit("SET_LIST", list);
+    if (options.save) {
+      window.electron.lowdb.set("projects", list);
+    }
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+};
