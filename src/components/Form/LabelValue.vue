673a9f98de5f19d764503cd69e940469c1634290
siche <siche@eos.pe.kr>
2025-05-08 11:05:47 +0900

    미등록 프로젝트 정리

---
+++ src/components/Form/LabelValue.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+q-item(clickable dense)
+  q-item-section(side :style="`width: ${width}; align-items: flex-end;`") {{ label }}
+  q-item-section {{ value || '-' }}
+</template>
+
+<script>
+export default {
+  name: "LabelValue",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: [String, Number],
+      default: "",
+    },
+    width: {
+      type: String,
+      default: "auto",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
