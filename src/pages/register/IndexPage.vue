1f89d90c7683b1a1e7519f5ca14a3251aaa137c5
siche <siche@eos.pe.kr>
2025-05-09 09:45:41 +0900

    feat(index): update page layout and content

--- src/pages/register/IndexPage.vue
+++ src/pages/register/IndexPage.vue
@@ -1,80 +1,81 @@
 <template lang="pug">
-q-card.layout
-  q-card-section(v-if="!path")
-    .row.justify-center.items-center(style="height: 100%;")
-      img(
-        src="@/assets/logo-1.png"
-        width="400px"
-      )
-  q-card-section(v-else)
-    .row.items-center.q-gutter-sm.q-mb-md
-      q-icon(
-        name="mdi-subtitles-outline"
-        size="30px"
-        color="white"
-      )
-      flat-input.text-h6.col(
-        v-model="projectName"
-        outlined
-      )
-      q-btn-dropdown(
-        color="white"
-        dropdown-icon="mdi-dots-vertical"
-        flat
-        no-icon-animation
-        content-class="context"
-      )
+q-page(:class="!path ? 'flex flex-center' : ''")
+  template(v-if="!path")
+    img(
+      src="@/assets/logo-1.png"
+      width="400px"
+    )
+  template(v-else)
+    q-card.layout
+      q-card-section
+        .row.items-center.q-gutter-sm.q-mb-md
+          q-icon(
+            name="mdi-subtitles-outline"
+            size="30px"
+            color="white"
+          )
+          flat-input.text-h6.col(
+            v-model="projectName"
+            outlined
+          )
+          q-btn-dropdown(
+            color="white"
+            dropdown-icon="mdi-dots-vertical"
+            flat
+            no-icon-animation
+            content-class="context"
+          )
+            q-list
+              q-item(
+                clickable
+                v-ripple
+                @click="clickOpenFinder"
+              )
+                q-icon.q-mr-sm(name="mdi-folder-open" size="20px")
+                | Finder 열기
+              q-item(
+                clickable
+                v-ripple
+                @click="clickkRegister"
+              )
+                q-icon.q-mr-sm(name="mdi-pill" size="20px")
+                | 프로젝트 등록
+              q-item(
+                clickable
+                v-ripple
+                @click="clickkRemove"
+              )
+                q-icon.q-mr-sm(name="mdi-package-variant-remove" size="20px")
+                | 해당 폴더 삭제
+
         q-list
-          q-item(
-            clickable
-            v-ripple
-            @click="clickOpenFinder"
-          )
-            q-icon.q-mr-sm(name="mdi-folder-open" size="20px")
-            | Finder 열기
-          q-item(
-            clickable
-            v-ripple
-            @click="clickkRegister"
-          )
-            q-icon.q-mr-sm(name="mdi-pill" size="20px")
-            | 프로젝트 등록
-          q-item(
-            clickable
-            v-ripple
-            @click="clickkRemove"
-          )
-            q-icon.q-mr-sm(name="mdi-package-variant-remove" size="20px")
-            | 해당 폴더 삭제
+          q-item-label(header :style="`width: ${labelWidth};`")
+            q-icon.q-mr-sm(name="mdi-pin" size="20px" color="white")
+            | 기본정보
+          label-value(label="경로" :value="info?.path" :width="labelWidth")
+          label-value(label="이름" :value="info?.name" :width="labelWidth")
+          label-value(label="버전" :value="info?.version" :width="labelWidth")
+          label-value(label="설명" :value="info?.description" :width="labelWidth")
+          label-value(label="라이센스" :value="info?.license" :width="labelWidth")
+          label-value(label="디렉토리 크기" :value="info?.size" :width="labelWidth")
 
-    q-list
-      q-item-label(header :style="`width: ${labelWidth};`")
-        q-icon.q-mr-sm(name="mdi-pin" size="20px" color="white")
-        | 기본정보
-      label-value(label="경로" :value="info?.path" :width="labelWidth")
-      label-value(label="이름" :value="info?.name" :width="labelWidth")
-      label-value(label="버전" :value="info?.version" :width="labelWidth")
-      label-value(label="설명" :value="info?.description" :width="labelWidth")
-      label-value(label="라이센스" :value="info?.license" :width="labelWidth")
-      label-value(label="디렉토리 크기" :value="info?.size" :width="labelWidth")
+          template(v-if="info?.git")
+            q-item-label(header :style="`width: ${labelWidth};`")
+              q-icon.q-mr-sm(name="mdi-git" size="20px" color="white")
+              | Git 정보
+            label-value(label="브랜치" :value="info?.git?.currentBranch" :width="labelWidth")
+            label-value(label="remote" :value="info?.git?.remotes[0]" :width="labelWidth")
+            label-value(label="마지막 커밋" :value="info?.git?.lastCommit.message" :width="labelWidth")
 
-      template(v-if="info?.git")
-        q-item-label(header :style="`width: ${labelWidth};`")
-          q-icon.q-mr-sm(name="mdi-git" size="20px" color="white")
-          | Git 정보
-        label-value(label="브랜치" :value="info?.git?.currentBranch" :width="labelWidth")
-        label-value(label="remote" :value="info?.git?.remotes[0]" :width="labelWidth")
-        label-value(label="마지막 커밋" :value="info?.git?.lastCommit.message" :width="labelWidth")
+          //- q-item-label(header :style="`width: ${labelWidth};`")
+            q-icon.q-mr-sm(name="mdi-book-multiple" size="20px" color="white")
+            | 의존성
 
-      //- q-item-label(header :style="`width: ${labelWidth};`")
-        q-icon.q-mr-sm(name="mdi-book-multiple" size="20px" color="white")
-        | 의존성
+          //- q-item-label(header :style="`width: ${labelWidth};`")
+            q-icon.q-mr-sm(name="mdi-file-document-multiple" size="20px" color="white")
+            | 구조
 
-      //- q-item-label(header :style="`width: ${labelWidth};`")
-        q-icon.q-mr-sm(name="mdi-file-document-multiple" size="20px" color="white")
-        | 구조
-
-      
+        
 </template>
 
 <script>
